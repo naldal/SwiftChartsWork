20fe8174204189e5373b66effaca913414a7a84d
russell <russell@wemade.com>
2022-11-14 14:45:59 +0900

    Initial Commit

---
+++ ChartWM/ChartWMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartWMApp.swift
+//  ChartWM
+//
+//  Created by WM-ID002346 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct ChartWMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartWMUITests/ChartWMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChartWMUITestsLaunchTests.swift
+//  ChartWMUITests
+//
+//  Created by WM-ID002346 on 2022/11/14.
+//
+
+import XCTest
+
+final class ChartWMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
